24821af39a0384adc6cda11e1d94b490db7f258b
ShubhShiv <46372759+ShubhShiv@users.noreply.github.com>
2020-04-15 11:26:34 +0530

    Basic Social Media Website(First Commit), Manual Authentication.

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
+
+app.use(express.static('./assets'));
+
+
+
 //Use Express Router
 app.use('/', require('./routes'));
 
